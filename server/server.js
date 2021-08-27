6637ba3413f31bbd57cd12c3d96504755cfa6a09
Matt Linder <mjlinder218@gmail.com>
2021-08-27 10:27:25 -0700

    Added express.static for public

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
