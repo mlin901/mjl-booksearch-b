98d8c62290672dfa1af7600726238fd58adc4e2f
Matt Linder <mjlinder218@gmail.com>
2021-08-29 15:27:41 -0700

    Initial commit

---
+++ server/utils/mutations.js
@@ -0,0 +1,62 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $userName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      userName: $firstName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        title 
+        authors
+      }
+    }
+  }
+`;
